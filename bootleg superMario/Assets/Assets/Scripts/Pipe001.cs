5d32e11a537e02d6186c93948a4ab46965f12070
Riham Abbady Ali <r_abbady@icloud.com>
2021-06-29 21:23:13 +0200

    go to the under ground through a pipe and change cameras

--- bootleg superMario/Assets/Assets/Scripts/Pipe001.cs
+++ bootleg superMario/Assets/Assets/Scripts/Pipe001.cs
@@ -1,55 +1,54 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// 
-using UnityStandardAssets.Characters.ThirdPerson;
-
 public class Pipe001 : MonoBehaviour
 {
-    public GameObject pipe_entry;
+
+    public GameObject PipeEntry;
     public int StoodOn;
 
+    public GameObject MainCam;
+    public GameObject SecondCam;
+    public GameObject MainPlayer;
 
-    private void OnTriggerEnter(Collider col)
+    void OnTriggerEnter(Collider col)
     {
         StoodOn = 1;
     }
 
-    private void OnTriggerExit(Collider col)
+    void OnTriggerExit(Collider col)
     {
         StoodOn = 0;
     }
 
-    IEnumerator WaitPipeAnimation()
+    // Use this for initialization
+    void Start()
     {
-        yield return new WaitForSeconds(2);
-        pipe_entry.GetComponent<Animator>().enabled = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("GoDown"))
+        if (Input.GetKey(KeyCode.S)||Input.GetKey(KeyCode.DownArrow))
         {
             if (StoodOn == 1)
             {
                 transform.position = new Vector3(0, -1000, 0);
-                WaitingForPipe();
+                StartCoroutine(WaitingForPipe());
             }
         }
     }
 
-    public void WaitingForPipe()
+    IEnumerator WaitingForPipe()
     {
-        pipe_entry.GetComponent<Animator>().enabled = true;
-        StartCoroutine("WaitPipeAnimation");
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-
+        PipeEntry.GetComponent<Animator>().enabled = true;
+        yield return new WaitForSeconds(2F);
+        PipeEntry.GetComponent<Animator>().enabled = false;
+        SecondCam.SetActive(true);
+        MainCam.SetActive(false);
+        MainPlayer.transform.position = new Vector3(11, -17, 0.5F);
     }
 }
